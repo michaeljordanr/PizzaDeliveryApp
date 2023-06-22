b3bc0d70eef5f5efcc7586246ef5090460361387
Michael Jordan <michaeljordanribeiro@gmail.com>
2023-06-21 22:29:33 -0300

    String format fix

--- app/src/main/java/com/michaeljordanr/pizzadelivery/common/DoubleExt.kt
+++ app/src/main/java/com/michaeljordanr/pizzadelivery/common/DoubleExt.kt
@@ -5,5 +5,9 @@ import java.text.DecimalFormat
 fun Double.currency(): String {
     val format = DecimalFormat("$#,###.00")
     format.isDecimalSeparatorAlwaysShown = false
-    return format.format(this).toString()
+    return if (this > 0) {
+        format.format(this).toString()
+    } else {
+        "$0.00"
+    }
 }
